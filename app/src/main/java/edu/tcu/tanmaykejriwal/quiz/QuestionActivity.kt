b83502dab4c61ea4664e247b6329d25005a3eb3b
tanmay-kali <tanmaykejriwal28@gmail.com>
2022-10-23 20:22:59 -0500

    Final

--- app/src/main/java/edu/tcu/tanmaykejriwal/quiz/QuestionActivity.kt
+++ app/src/main/java/edu/tcu/tanmaykejriwal/quiz/QuestionActivity.kt
@@ -1,5 +1,6 @@
 package edu.tcu.tanmaykejriwal.quiz
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,45 +12,91 @@ import androidx.core.content.ContextCompat
 class QuestionActivity : AppCompatActivity(), View.OnClickListener {
 
     private val questions = Constants.getQuestions().shuffled()
-    private var questionIdx = 0
+    private var questionIdx =0
     private val optionTVs = mutableListOf<TextView>()
     private var selectedOptionIdx = -1
     private var answerReveal = false
-    private var correctanswer = 0
-    private var correctText =""
+    private var correctanswer = ""
+    private var chosen: TextView? =null;
+    private var score= 0
+    private var name = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-
-        val username = intent.getIntExtra("username",0)
+        name = intent.getStringExtra("username").toString()
+        println(name)
         findViewById<ProgressBar>(R.id.bar).max = questions.size
-        findViewById<TextView>(R.id.question_tv).text = "WHICH COUNTRY DOES THIS FLAG BELONG TO"
+        findViewById<TextView>(R.id.question_tv).text =questions[0].question
         findViewById<ImageView>(R.id.flag_iv).setImageResource(R.drawable.ic_flag_of_australia)
-        findViewById<Button>(R.id.button).setOnClickListener(this)
+        findViewById<Button>(R.id.button).setOnClickListener{movealong()}
+        createQuestion();
 
+
+
+
+    }
+
+    private fun createQuestion(){
+        findViewById<Button>(R.id.button).setOnClickListener{movealong()}
+        findViewById<Button>(R.id.button).text="Submit"
+        chosen = null;
+        var ind = questionIdx
+        var getOptions = questions[ind].options.shuffled()
         var optionll = findViewById<LinearLayout>(R.id.option_ll)
+        var temp = ind+1
+        findViewById<ProgressBar>(R.id.bar).progress=temp
+        findViewById<TextView>(R.id.barNumber).text= temp.toString()+ "/10"
+        findViewById<ImageView>(R.id.flag_iv).setImageResource(questions[ind].image)
+        correctanswer=questions[ind].correctAnswer
+        optionll.removeAllViews()
+        for(item in getOptions){
+            val opt=createOption(item)
+            optionll.addView(opt)
+        }
+        questionIdx++
+    }
 
-        val optionTv = TextView(this);
-        val layoutparams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        layoutparams.topMargin = dpToPx(16);
-        optionTv.layoutParams = layoutparams
-        optionTv.gravity = Gravity.CENTER
-        optionTv.setPadding(dpToPx(16),dpToPx(16),dpToPx(16),dpToPx(16))
-        optionTv.textSize = 16F
-        optionTv.text = "TEST"
-        optionTv.setTextColor(ContextCompat.getColor(this, R.color.gray))
-        optionTv.typeface = Typeface.DEFAULT
-        optionTv.setBackgroundResource(R.drawable.default_option_bg)
-        val secopt=createOption("TK")
-        val thirdopt=createOption("TM")
+    private fun movealong(){
 
-        optionll.addView(optionTv)
-        optionll.addView(secopt)
-        optionll.addView(thirdopt)
+        if(chosen==null){
+            Toast.makeText(this, "Please make a selection", Toast.LENGTH_SHORT).show()
+        }
 
+        else{
+            if(chosen!!.text==correctanswer){
+                chosen!!.setBackgroundResource(R.drawable.correct_option_bg)
+                score++
+            }
+            else{
+                chosen!!.setBackgroundResource(R.drawable.wrong_option_bg)
+                for (optionTv in optionTVs){
+                    if(optionTv.text==correctanswer){
+                        optionTv.setBackgroundResource(R.drawable.correct_option_bg)
+                    }
+                }
+            }
+
+            for (optionTv in optionTVs){
+                optionTv.setOnClickListener(null)
+            }
+            if (questionIdx>=questions.size){
+                goToResult()
+            }
+            else {
+                findViewById<Button>(R.id.button).text = "Go to Next Question"
+                findViewById<Button>(R.id.button).setOnClickListener { createQuestion() }
+            }
+        }
+
+
+    }
+
+    private fun goToResult(){
+        val intent = Intent(this,ResultActivity::class.java)
+        intent.putExtra("score",score.toString())
+        intent.putExtra("name",name)
+        startActivity(intent)
+        finish()
     }
 
     override fun onClick(p0: View?) {
@@ -76,6 +123,7 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener {
         optionTv.setTextColor(ContextCompat.getColor(this, R.color.gray))
         optionTv.typeface = Typeface.DEFAULT
         optionTv.setBackgroundResource(R.drawable.default_option_bg)
+        optionTVs.add(optionTv)
         optionTv.setOnClickListener{setOptionView(optionTv)}
         return optionTv
 
@@ -90,6 +138,7 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener {
         selectedOption.setTextColor(ContextCompat.getColor(this,R.color.black))
         selectedOption.typeface = Typeface.DEFAULT_BOLD
         selectedOption.setBackgroundResource(R.drawable.selected_option_bg)
+        chosen= selectedOption
 
     }
 
--- app/src/main/java/edu/tcu/tanmaykejriwal/quiz/ResultActivity.kt
+++ app/src/main/java/edu/tcu/tanmaykejriwal/quiz/ResultActivity.kt
@@ -1,11 +1,45 @@
 package edu.tcu.tanmaykejriwal.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
+private var name = ""
+private var score = ""
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        name= intent.getStringExtra("name").toString()
+        score = intent.getStringExtra("score").toString()
+
+        var emoji = findViewById<ImageView>(R.id.emoji)
+        var scoreText =  findViewById<TextView>(R.id.score)
+        var textM =  findViewById<TextView>(R.id.congrats)
+        var tryagain =  findViewById<Button>(R.id.tryagain)
+
+        if(score.toInt()>5){
+            emoji.setImageResource(R.drawable.ic_trophy)
+            scoreText.text="You scored "+ score+" out of 10"
+            textM.text="Congratulations, "+ name
+        }
+        else{
+            emoji.setImageResource(R.drawable.ic_sweat_face)
+            scoreText.text="You scored "+ score+" out of 10"
+            textM.text="Good Luck Next Time, "+ name
+        }
+        tryagain.setOnClickListener{
+            gotoStart()
+        }
+
+    }
+    private fun gotoStart(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
     }
 }
